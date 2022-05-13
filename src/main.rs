494b2af6640f576ad1dd67a25c2dce14be4f1810
ｈｉｌｊｕｓｔｉ <hiljusti@so.dang.cool>
2022-05-13 02:08:08 -0700

    Refactor parsing

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,13 @@ fn main() {
 
     let mut stack: Vec<i64> = vec![];
 
+    let mut dictionary = new_dictionary();
+
     loop {
         let input = editor.readline("> ");
 
         if let Err(e) = input {
+            eprintln!("Final state: {:?}", stack);
             eprintln!("Derailed: {:?}", e);
             std::process::exit(1);
         }
@@ -17,62 +20,110 @@ fn main() {
 
         editor.add_history_entry(&input);
 
-        let term = RailTerm::from_str(&input);
+        if let Some(op) = dictionary.iter_mut().find(|op| op.name == input) {
+            op.go(&mut stack);
+        } else if let Ok(i) = input.parse::<i64>() {
+            stack.push(i);
+        } else {
+            eprintln!("Derailed: unknown term {:?}", input);
+        }
+    }
+}
 
-        match term {
-            RailTerm::Dot => println!("{:?}", stack),
-            RailTerm::Int(i) => stack.push(i),
-            RailTerm::MathOp(op) => {
-                if stack.len() < 2 {
-                    eprintln!("Derailed: stack underflow {:?}", stack);
-                    std::process::exit(1);
-                }
+type Stack = Vec<i64>;
+
+struct RailOp {
+    name: String,
+    consumes: Vec<String>,
+    produces: Vec<String>,
+    op: Box<dyn FnMut(&mut Stack)>,
+}
+
+impl RailOp {
+    fn go(&mut self, stack: &mut Stack) {
+        if stack.len() < self.consumes.len() {
+            // TODO: At some point will want source context here like line/column number.
+            eprintln!(
+                "Derailed: stack underflow for \"{}\" ({:?} -> {:?}): stack only had {}",
+                self.name,
+                self.consumes,
+                self.produces,
+                stack.len()
+            );
+            std::process::exit(1);
+        }
+
+        (self.op)(stack);
+    }
+}
+
+fn new_dictionary() -> Vec<RailOp> {
+    vec![
+        RailOp {
+            name: String::from("."),
+            consumes: vec![],
+            produces: vec![],
+            op: Box::new(|stack| println!("{:?}", stack)),
+        },
+        RailOp {
+            name: String::from("+"),
+            consumes: vec![String::from("i64"), String::from("i64")],
+            produces: vec![String::from("i64")],
+            op: binary_op(|a, b| a + b),
+        },
+        RailOp {
+            name: String::from("-"),
+            consumes: vec![String::from("i64"), String::from("i64")],
+            produces: vec![String::from("i64")],
+            op: binary_op(|a, b| a - b),
+        },
+        RailOp {
+            name: String::from("*"),
+            consumes: vec![String::from("i64"), String::from("i64")],
+            produces: vec![String::from("i64")],
+            op: binary_op(|a, b| a * b),
+        },
+        RailOp {
+            name: String::from("/"),
+            consumes: vec![String::from("i64"), String::from("i64")],
+            produces: vec![String::from("i64")],
+            op: binary_op(|a, b| a / b),
+        },
+        RailOp {
+            name: String::from("swap"),
+            consumes: vec![String::from("b"), String::from("a")],
+            produces: vec![String::from("a"), String::from("b")],
+            op: Box::new(|stack| {
                 let a = stack.pop().unwrap();
                 let b = stack.pop().unwrap();
-                stack.push(match op {
-                    RailMathOp::Plus => a + b,
-                    RailMathOp::Minus => a - b,
-                    RailMathOp::Times => a * b,
-                    RailMathOp::DivBy => a / b,
-                })
-            }
-            RailTerm::Unknown(term) => {
-                eprintln!("Derailed: unknown term {:?}", term);
-                std::process::exit(1);
-            }
-        }
-    }
+                stack.push(a);
+                stack.push(b);
+            }),
+        },
+        RailOp {
+            name: String::from("rot"),
+            consumes: vec![String::from("c"), String::from("b"), String::from("a")],
+            produces: vec![String::from("a"), String::from("c"), String::from("b")],
+            op: Box::new(|stack| {
+                let a = stack.pop().unwrap();
+                let b = stack.pop().unwrap();
+                let c = stack.pop().unwrap();
+                stack.push(a);
+                stack.push(c);
+                stack.push(b);
+            }),
+        },
+    ]
 }
 
-enum RailTerm {
-    Dot,
-    Int(i64),
-    MathOp(RailMathOp),
-    Unknown(String),
-}
-
-enum RailMathOp {
-    Plus,
-    Minus,
-    Times,
-    DivBy,
-}
-
-impl RailTerm {
-    // TODO: Make a parser yo
-    fn from_str(s: &str) -> Self {
-        use RailMathOp::*;
-        use RailTerm::*;
-        match s {
-            "." => Dot,
-            "+" => MathOp(Plus),
-            "-" => MathOp(Minus),
-            "*" => MathOp(Times),
-            "/" => MathOp(DivBy),
-            _ => match s.parse::<i64>() {
-                Ok(i) => Int(i),
-                Err(_) => Unknown(s.to_string()),
-            },
-        }
-    }
+fn binary_op<'a, F>(op: F) -> Box<dyn FnMut(&mut Stack) + 'a>
+where
+    F: Fn(i64, i64) -> i64 + Sized + 'a,
+{
+    Box::new(move |stack: &mut Stack| {
+        let a = stack.pop().unwrap();
+        let b = stack.pop().unwrap();
+        let c = op(a, b);
+        stack.push(c);
+    })
 }
