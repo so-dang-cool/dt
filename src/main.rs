280944fcd3848ceac91d4276e201cbfb517be0f3
ｈｉｌｊｕｓｔｉ <hiljusti@so.dang.cool>
2022-05-27 23:00:33 -0700

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::sync::Arc;
 use rustyline::Editor;
 
 pub const RAIL_VERSION: &str = std::env!("CARGO_PKG_VERSION");
@@ -7,9 +8,11 @@ fn main() {
 
     let mut editor = Editor::<()>::new();
 
-    let mut stack: Stack = Stack::new();
+    let mut stack = Stack::new();
 
     let mut dictionary = new_dictionary();
+
+    let mut saving = false;
 
     loop {
         let input = editor.readline("> ");
@@ -27,25 +30,53 @@ fn main() {
         let terms = input.split_whitespace().collect::<Vec<_>>();
 
         for term in terms {
-            if let Some(op) = dictionary.iter_mut().find(|op| op.name == term) {
-                op.go(&mut stack);
-            } else if let (Some('"'), Some('"')) = (term.chars().next(), term.chars().last()) {
-                let s = term.chars().skip(1).take(term.len() - 2).collect();
-                stack.push(RailTerm::String(s));
-            } else if let Ok(i) = term.parse::<i64>() {
-                stack.push(RailTerm::I64(i));
+            // TODO: not sufficient to have nested substacks yet.
+            if saving {
+                if let "]" = term {
+                    saving = false;
+
+                    let mut substack = Stack::new();
+                    let mut next = || stack.pop().expect("Did not find \"[\" to begin a substack.");
+                    let mut curr = next();
+                
+                    while curr != RailTerm::SubstackStart {
+                        substack.pushl(curr);
+                        curr = next();
+                    }
+                
+                    stack.push(RailTerm::Substack(substack))
+                } else {
+                    stack.push(RailTerm::Unparsed(term.to_string()));
+                }
+            } else if let "[" = term {
+                saving = true;
+                stack.push(RailTerm::SubstackStart);
             } else {
-                eprintln!("Derailed: unknown term {:?}", term);
-                std::process::exit(1);
+                let from_dict = dictionary.iter().find(|op| op.name == term);
+                if let Some(op) = from_dict {
+                    let mut op = op.clone();
+                    op.go(&mut stack, &mut dictionary);
+                } else if let (Some('"'), Some('"')) = (term.chars().next(), term.chars().last()) {
+                    let s = term.chars().skip(1).take(term.len() - 2).collect();
+                    stack.push(RailTerm::String(s));
+                } else if let Ok(i) = term.parse::<i64>() {
+                    stack.push(RailTerm::I64(i));
+                } else {
+                    eprintln!("Derailed: unknown term {:?}", term);
+                    std::process::exit(1);
+                }
             }
         }
     }
 }
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq)]
 enum RailTerm {
     I64(i64),
     String(String),
+    SubstackStart,
+    Unparsed(String),
+    Substack(Stack),
 }
 
 impl std::fmt::Display for RailTerm {
@@ -54,10 +85,14 @@ impl std::fmt::Display for RailTerm {
         match self {
             I64(n) => write!(fmt, "{:?}", n),
             String(s) => write!(fmt, "\"{}\"", s),
+            SubstackStart => write!(fmt, "["),
+            Unparsed(s) => write!(fmt, "{}", s),
+            Substack(s) => write!(fmt, "({:?})", s),
         }
     }
 }
 
+#[derive(Clone, Debug, PartialEq)]
 struct Stack {
     terms: Vec<RailTerm>,
 }
@@ -69,6 +104,10 @@ impl Stack {
 
     fn push(&mut self, term: RailTerm) {
         self.terms.push(term)
+    }
+
+    fn pushl(&mut self, term: RailTerm) {
+        self.terms.insert(0, term)
     }
 
     fn len(&self) -> usize {
@@ -94,52 +133,71 @@ struct RailOp<'a> {
     name: &'a str,
     consumes: &'a [&'a str],
     produces: &'a [&'a str],
-    op: Box<dyn FnMut(&mut Stack) + 'a>,
+    op: Arc<dyn FnMut(&mut Stack) + 'static>,
 }
 
 impl RailOp<'_> {
-    fn new<'a, F>(
+    fn new<'a>(
         name: &'a str,
         consumes: &'a [&'a str],
         produces: &'a [&'a str],
-        op: F,
+        op: Arc<dyn FnMut(&mut Stack) + 'static>,
     ) -> RailOp<'a>
-    where
-        F: FnMut(&mut Stack) + 'a,
     {
         RailOp {
             name,
             consumes,
             produces,
-            op: Box::new(op),
+            op,
         }
     }
 
-    fn go(&mut self, stack: &mut Stack) {
+    fn new_raw<'a, F>(
+        name: &'a str,
+        consumes: &'a [&'a str],
+        produces: &'a [&'a str],
+        op: F,
+    ) -> RailOp<'a>
+        where F: FnMut(&mut Stack) + 'static
+     
+    {
+        RailOp {
+            name,
+            consumes,
+            produces,
+            op: Arc::new(op),
+        }
+    }
+
+    fn go(&mut self, stack: &mut Stack, dictionary: &mut Dictionary) {
         if stack.len() < self.consumes.len() {
             // TODO: At some point will want source context here like line/column number.
             eprintln!(
-                "Derailed: stack underflow for \"{}\" ({:?} -> {:?}): stack only had {}",
+                "Derailed: stack underflow for \"{}\" ({} -> {}) -- wanted {} but stack only had {}",
                 self.name,
-                self.consumes,
-                self.produces,
+                self.consumes.join(" "),
+                self.produces.join(" "),
+                self.consumes.len(),
                 stack.len()
             );
             std::process::exit(1);
         }
 
         // TODO: Type checks
+        let mut op = &self.op;
 
-        (self.op)(stack);
+        op(stack);
     }
 }
 
-fn new_dictionary() -> Vec<RailOp<'static>> {
+type Dictionary = Vec<RailOp<'static>>;
+
+fn new_dictionary() -> Dictionary {
     vec![
-        RailOp::new(".", &["a"], &[], |stack| {
+        RailOp::new_raw(".", &["a"], &[], |stack| {
             println!("{:?}", stack.pop().unwrap())
         }),
-        RailOp::new(".s", &[], &[], |stack| println!("{}", stack)),
+        RailOp::new_raw(".s", &[], &[], |stack| println!("{}", stack)),
         RailOp::new("+", &["i64", "i64"], &["i64"], binary_op(|a, b| a + b)),
         RailOp::new("-", &["i64", "i64"], &["i64"], binary_op(|a, b| a - b)),
         RailOp::new("*", &["i64", "i64"], &["i64"], binary_op(|a, b| a * b)),
@@ -165,21 +223,21 @@ fn new_dictionary() -> Vec<RailOp<'static>> {
             &["i64"],
             binary_op(|a, b| if a <= b { a } else { b }),
         ),
-        RailOp::new("drop", &["a"], &[], |stack| {
+        RailOp::new_raw("drop", &["a"], &[], |stack| {
             stack.pop().unwrap();
         }),
-        RailOp::new("dup", &["a"], &["a", "a"], |stack| {
+        RailOp::new_raw("dup", &["a"], &["a", "a"], |stack| {
             let a = stack.pop().unwrap();
             stack.push(a.clone());
             stack.push(a);
         }),
-        RailOp::new("swap", &["b", "a"], &["a", "b"], |stack| {
+        RailOp::new_raw("swap", &["b", "a"], &["a", "b"], |stack| {
             let a = stack.pop().unwrap();
             let b = stack.pop().unwrap();
             stack.push(a);
             stack.push(b);
         }),
-        RailOp::new("rot", &["c", "b", "a"], &["a", "c", "b"], |stack| {
+        RailOp::new_raw("rot", &["c", "b", "a"], &["a", "c", "b"], |stack| {
             let a = stack.pop().unwrap();
             let b = stack.pop().unwrap();
             let c = stack.pop().unwrap();
@@ -192,11 +250,11 @@ fn new_dictionary() -> Vec<RailOp<'static>> {
 
 // Operations
 
-fn unary_op<'a, F>(op: F) -> Box<dyn FnMut(&mut Stack) + 'a>
+fn unary_op<'a, F>(op: F) -> Arc<dyn FnMut(&mut Stack) + 'a>
 where
     F: Fn(i64) -> i64 + Sized + 'a,
 {
-    Box::new(move |stack: &mut Stack| {
+    Arc::new(move |stack: &mut Stack| {
         let a = stack.pop().unwrap();
         match a {
             RailTerm::I64(a) => {
@@ -208,11 +266,11 @@ where
     })
 }
 
-fn binary_op<'a, F>(op: F) -> Box<dyn FnMut(&mut Stack) + 'a>
+fn binary_op<'a, F>(op: F) -> Arc<dyn FnMut(&mut Stack) + 'a>
 where
     F: Fn(i64, i64) -> i64 + Sized + 'a,
 {
-    Box::new(move |stack: &mut Stack| {
+    Arc::new(move |stack: &mut Stack| {
         let a = stack.pop().unwrap();
         let b = stack.pop().unwrap();
         match (a, b) {
@@ -227,11 +285,11 @@ where
 
 // Predicates
 
-fn unary_pred<'a, F>(op: F) -> Box<dyn FnMut(&mut Stack) + 'a>
+fn unary_pred<'a, F>(op: F) -> Arc<dyn FnMut(&mut Stack) + 'a>
 where
     F: Fn(i64) -> bool + Sized + 'a,
 {
-    Box::new(move |stack: &mut Stack| {
+    Arc::new(move |stack: &mut Stack| {
         let a = stack.pop().unwrap();
         match a {
             RailTerm::I64(a) => {
@@ -243,11 +301,11 @@ where
     })
 }
 
-fn binary_pred<'a, F>(op: F) -> Box<dyn FnMut(&mut Stack) + 'a>
+fn binary_pred<'a, F>(op: F) -> Arc<dyn FnMut(&mut Stack) + 'a>
 where
     F: Fn(i64, i64) -> bool + Sized + 'a,
 {
-    Box::new(move |stack: &mut Stack| {
+    Arc::new(move |stack: &mut Stack| {
         let a = stack.pop().unwrap();
         let b = stack.pop().unwrap();
         match (a, b) {
