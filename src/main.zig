e2aa164fd0421d89c04f9c9f2373e9dbc75b709a
ｈｉｌｊｕｓｔｉ <hiljusti@so.dang.cool>
2022-05-30 22:30:53 -0700

    Move cli to its own source

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,15 @@
 const std = @import("std");
+const stdout = std.io.getStdOut().writer();
+const stderr = std.io.getStdErr().writer();
+
+const railCli = @import("rail/cli.zig");
+
+const version = "0.1.0";
 
 pub fn main() anyerror!void {
-    std.log.info("All your codebase are belong to us.", .{});
+    const alloc = std.heap.page_allocator;
+    const runSettings = try railCli.initialize(alloc);
+    try stdout.print("{s} {s}\n", .{runSettings.program, version});
 }
 
 test "basic test" {
