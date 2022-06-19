91a29f7d95c52afcc96d6645d6a33263de014d2f
ｈｉｌｊｕｓｔｉ <hiljusti@so.dang.cool>
2022-06-19 01:37:54 -0700

    Add 'times' to corelib

--- src/corelib.rs
+++ src/corelib.rs
@@ -124,6 +124,18 @@ pub fn new_dictionary() -> Dictionary {
                 context: state.context,
             }
         }),
+        RailOp::new("times", &["quot", "i64"], &[], |state| {
+            let mut stack = state.stack.clone();
+            let n = match stack.pop().unwrap() {
+                RailVal::I64(n) => n,
+                rail_val => panic!("def requires an integer, but got {:?}", rail_val),
+            };
+            let quot = match stack.pop() {
+                Some(RailVal::Quotation(quot)) => quot,
+                rail_val => panic!("def requires a quotation, but got {:?}", rail_val),
+            };
+            (1..n).fold(state.update_stack(stack), |state, _n| run_quot(&quot, state))
+        }),
     ]
 }
 
