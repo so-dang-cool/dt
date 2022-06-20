735bb27d816e41b8b5f8a41adecda961053cf15e
ｈｉｌｊｕｓｔｉ <hiljusti@so.dang.cool>
2022-06-19 21:17:49 -0700

    Add push/pop, bump to 0.4.1

--- src/corelib.rs
+++ src/corelib.rs
@@ -186,6 +186,28 @@ pub fn new_dictionary() -> Dictionary {
             stack.push(RailVal::I64(len));
             state.update_stack(stack)
         }),
+        RailOp::new("push", &["quot", "a"], &["quot"], |state| {
+            let mut stack = state.stack.clone();
+            let a = stack.pop().unwrap();
+            let mut quot = match stack.pop().unwrap() {
+                RailVal::Quotation(quot) => quot,
+                rail_val => panic!("len requires a quotation, but got {:?}", rail_val),
+            };
+            quot.push(a);
+            stack.push(RailVal::Quotation(quot));
+            state.update_stack(stack)
+        }),
+        RailOp::new("pop", &["quot"], &["quot", "a"], |state| {
+            let mut stack = state.stack.clone();
+            let mut quot = match stack.pop().unwrap() {
+                RailVal::Quotation(quot) => quot,
+                rail_val => panic!("len requires a quotation, but got {:?}", rail_val),
+            };
+            let a = quot.pop().unwrap();
+            stack.push(RailVal::Quotation(quot));
+            stack.push(a);
+            state.update_stack(stack)
+        }),
     ]
 }
 
