ff9a45ce5782fa510e9894513853123dadf81199
ｈｉｌｊｕｓｔｉ <hiljusti@so.dang.cool>
2022-06-22 22:58:21 -0700

    Add 'stack/concat' and end-to-end tests

--- src/corelib/stack.rs
+++ src/corelib/stack.rs
@@ -1,5 +1,7 @@
+use crate::Stack;
 use crate::RailOp;
 
+// TODO: Should all these work for a String too? Should String also be a stack?
 pub fn builtins() -> Vec<RailOp<'static>> {
     vec![
         RailOp::new("len", &["quot"], &["i64"], |state| {
@@ -19,7 +21,7 @@ pub fn builtins() -> Vec<RailOp<'static>> {
         }),
         RailOp::new("pop", &["quot"], &["quot", "a"], |state| {
             let mut stack = state.stack.clone();
-            let mut quot = stack.pop_quotation("push");
+            let mut quot = stack.pop_quotation("pop");
             let a = quot.pop().unwrap();
             stack.push_quotation(quot);
             stack.push(a);
@@ -27,8 +29,18 @@ pub fn builtins() -> Vec<RailOp<'static>> {
         }),
         RailOp::new("rev", &["quot"], &["quot"], |state| {
             let mut stack = state.stack.clone();
-            let mut quot = stack.pop_quotation("push");
+            let mut quot = stack.pop_quotation("rev");
             quot.terms.reverse();
+            stack.push_quotation(quot);
+            state.update_stack(stack)
+        }),
+        RailOp::new("concat", &["quot", "quot"], &["quot"], |state| {
+            let mut stack = state.stack.clone();
+            let quot_b = stack.pop_quotation("concat");
+            let quot_a = stack.pop_quotation("concat");
+            let mut quot = Stack::new();
+            quot_a.terms.into_iter().for_each(|term| quot.push(term));
+            quot_b.terms.into_iter().for_each(|term| quot.push(term));
             stack.push_quotation(quot);
             state.update_stack(stack)
         }),
---
+++ tests/interpreter_basic.rs
@@ -0,0 +1,73 @@
+use std::io::{Read, Write};
+use std::process::{Command, Stdio};
+
+pub const RAIL_PATH: &str = std::env!("CARGO_BIN_EXE_rail");
+
+#[test]
+pub fn one_plus_one_is_two() {
+    let res = rail_interpret("1 1 +\n");
+
+    assert!(res.stdout.ends_with("[ 2 ]\n"));
+    assert_eq!("Derailed: End of input\n", res.stderr);
+}
+
+#[test]
+pub fn one_plus_one_is_still_two() {
+    let res = rail_interpret("1 1 [ + ] call\n");
+
+    assert!(res.stdout.ends_with("[ 2 ]\n"));
+    assert_eq!("Derailed: End of input\n", res.stderr);
+}
+
+#[test]
+pub fn one_plus_one_is_definitely_two() {
+    let res = rail_interpret("1 [ 1 + ] call\n");
+
+    assert!(res.stdout.ends_with("[ 2 ]\n"));
+    assert_eq!("Derailed: End of input\n", res.stderr);
+}
+
+#[test]
+pub fn one_plus_one_is_positively_two() {
+    let res = rail_interpret("[ 1 ] 2 times +\n");
+
+    assert!(res.stdout.ends_with("[ 2 ]\n"));
+    assert_eq!("Derailed: End of input\n", res.stderr);
+}
+
+#[test]
+pub fn one_plus_one_is_never_not_two() {
+    let res = rail_interpret("[ 1 ] [ 1 ] [ + ] [ concat ] 2 times call\n");
+
+    assert!(res.stdout.ends_with("[ 2 ]\n"));
+    assert_eq!("Derailed: End of input\n", res.stderr);
+}
+
+pub struct RailRunResult {
+    stdout: String,
+    stderr: String,
+}
+
+fn rail_interpret(stdin: &str) -> RailRunResult {
+    let rail_proc = Command::new(RAIL_PATH)
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .spawn()
+        .expect("Error running process");
+    
+        rail_proc
+        .stdin
+        .expect("Error sending stdin")
+        .write_all(stdin.as_bytes())
+        .unwrap();
+    
+
+    let mut stdout = String::new();
+    rail_proc.stdout.unwrap().read_to_string(&mut stdout).unwrap();
+
+    let mut stderr = String::new();
+    rail_proc.stderr.unwrap().read_to_string(&mut stderr).unwrap();
+
+    RailRunResult { stdout, stderr }
+}
