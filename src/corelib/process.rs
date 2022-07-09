2b15609697f9730e173c20a0e575f952d27fa729
ｈｉｌｊｕｓｔｉ <hiljusti@so.dang.cool>
2022-07-08 23:01:30 -0700

    Add env/envget/envset

--- src/corelib/process.rs
+++ src/corelib/process.rs
@@ -1,11 +1,10 @@
+use std::env;
+
 use crate::rail_machine::{Quote, RailDef};
 
 pub fn builtins() -> Vec<RailDef<'static>> {
-    vec![RailDef::on_quote(
-        "exec",
-        &["string"],
-        &["quote"],
-        |quote| {
+    vec![
+        RailDef::on_quote("exec", &["string"], &["quote"], |quote| {
             let (invocation, quote) = quote.pop_string("exec");
             let invocation = invocation.trim();
             let (exe, args) = invocation.split_once(' ').unwrap_or((invocation, ""));
@@ -19,6 +18,23 @@ pub fn builtins() -> Vec<RailDef<'static>> {
                 .push_str(String::from_utf8(res.stderr).unwrap().trim_end());
 
             quote.push_quote(result)
-        },
-    )]
+        }),
+        RailDef::on_quote("env", &[], &["string"], |quote| {
+            let vars = env::vars()
+                .map(|(k, v)| Quote::default().push_string(k).push_string(v))
+                .fold(Quote::default(), |q, kv| q.push_quote(kv));
+            quote.push_quote(vars)
+        }),
+        RailDef::on_quote("envget", &["string"], &["string"], |quote| {
+            let (key, quote) = quote.pop_string("envget");
+            let var = env::var(key).unwrap_or_else(|_| "unset".to_string());
+            quote.push_string(var)
+        }),
+        RailDef::on_quote("envset", &["string", "string"], &[], |quote| {
+            let (var, quote) = quote.pop_string("envset");
+            let (key, quote) = quote.pop_string("envset");
+            env::set_var(key, var);
+            quote
+        }),
+    ]
 }
