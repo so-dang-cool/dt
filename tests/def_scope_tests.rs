e7f7ba4440d14e7a34808f288cf9f443c950b898
ｈｉｌｊｕｓｔｉ <hiljusti@so.dang.cool>
2022-10-28 21:36:58 -0700

    Bump rail to 0.23.0, reformat many things

--- tests/def_scope_tests.rs
+++ tests/def_scope_tests.rs
@@ -4,9 +4,9 @@ use dt_test_utils::dt;
 #[test]
 fn basic_def() {
     let source = r#"
-        [ 1 + ] "inc" def
+        [1 +] [inc] def
         1 inc print
-        "inc" def? print
+        [inc] def? print
     "#;
 
     assert_eq!("2true", dt(&[source]).stdout);
@@ -15,7 +15,7 @@ fn basic_def() {
 #[test]
 fn basic_do_bang_def() {
     let source = r#"
-        [ [ ] "empty-quote" def ] do!
+        [[] [empty-quote] def] do!
 
         "empty-quote" def? "do! should define in parent context" assert-true
     "#;
@@ -27,7 +27,7 @@ fn basic_do_bang_def() {
 #[test]
 fn basic_do_def() {
     let source = r#"
-        [ [ ] "empty-quote" def ] do
+        [[] [empty-quote] def] do
 
         "empty-quote" undef? "do should NOT define in parent context" assert-true
     "#;
@@ -42,7 +42,7 @@ fn basic_arrow_do() {
         3 "banana"
 
         # Print banana three times
-        [ [ "n" "str" ] -> [ str print ] n times ] do
+        [[n str]: [str print] n times] do
 
         "n" undef? "do must not leak definitions, but n was defined" assert-true
         "str" undef? "do must not leak definitions, but str was defined" assert-true
@@ -59,7 +59,7 @@ fn basic_arrow_do() {
 fn arrow_in_times() {
     let source = r#"
         1
-        [ [ n ] ->
+        [ [ n ]:
             n println
             n 2 *
         ] 7 times
@@ -81,7 +81,7 @@ fn arrow_in_times() {
 fn arrow_in_each() {
     let source = r#"
         [ 1 2 3 4 5 ]
-        [ [ n ] -> n n * println ] each
+        [ [ n ]: n n * println ] each
 
         [ n ] undef? "each must not leak definitions, but n was defined" assert-true
     "#;
@@ -96,10 +96,10 @@ fn arrow_in_each() {
 #[test]
 fn arrow_in_map() {
     let source = r#"
-        [ "apple" "banana" "cereal" ]
-        [ [ food ] -> food upcase ] map print
+        ["apple" "banana" "cereal"]
+        [[food]: food upcase] map print
 
-        "food" undef? "map must not leak definitions, but food was defined" assert-true
+        [food] undef? "map must not leak definitions, but food was defined" assert-true
     "#;
 
     let res = dt(&[source]);
@@ -112,11 +112,11 @@ fn arrow_in_map() {
 #[test]
 fn arrow_in_filter() {
     let source = r#"
-        [ [ 1 "banana" ] [ 2 "banana" ] [ 3 "banana" ] [ 4 "bananas make a bunch!" ] ]
-        [ ... [ n str ] -> n even? str len odd? ] filter unquote print
+        [[1 "banana"] [2 "banana"] [3 "banana"] [4 "bananas make a bunch!"]]
+        [...[n str]: n even? str len odd?] filter unquote print
 
-        "n" undef? "filter must not leak definitions, but n was defined" assert-true
-        "str" undef? "filter must not leak definitions, but str was defined" assert-true
+        [n]   undef? "filter must not leak definitions, but n was defined" assert-true
+        [str] undef? "filter must not leak definitions, but str was defined" assert-true
     "#;
 
     let res = dt(&[source]);
@@ -155,7 +155,7 @@ fn shadowing_arrow_in_do() {
 
         fav-number println
 
-        2 [ [ fav-number ] -> fav-number println ] do
+        2 [ [ fav-number ] : fav-number println ] do
 
         fav-number println
     "#;
@@ -172,7 +172,7 @@ fn shadowing_in_times() {
     let source = r#"
         [ 1 ] [ n ] def
 
-        n [ [ n ] -> n println n 1 + ] 3 times
+        n [ [ n ] : n println n 1 + ] 3 times
 
         n println
     "#;
@@ -191,7 +191,7 @@ fn shadowing_in_each() {
 
         pizza println
 
-        [ "cheese" "bbq" "combo" ] [ [ pizza ] -> pizza println ] each
+        [ "cheese" "bbq" "combo" ] [ [ pizza ] : pizza println ] each
 
         pizza println
     "#;
@@ -213,7 +213,7 @@ fn shadowing_in_map() {
 
         x println
 
-        [ 1 2 3 ] [ [ x ] -> x 2.0 / ] map println
+        [ 1 2 3 ] [ [ x ]: x 2.0 / ] map println
 
         x println
     "#;
@@ -235,7 +235,7 @@ fn shadowing_in_filter() {
 
         happy-word println
 
-        [ "yay" "hurray" "whoo" "huzzah" ] [ [ happy-word ] -> 4 happy-word len gt? ] filter println
+        [ "yay" "hurray" "whoo" "huzzah" ] [ [ happy-word ] : 4 happy-word len gt? ] filter println
 
         happy-word println
     "#;
