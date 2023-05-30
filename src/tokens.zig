7ca7a62e81cbc80df1ad687e3645f9c490abe9a6
ｈｉｌｊｕｓｔｉ <hiljusti@so.dang.cool>
2023-05-30 07:42:31 -0700

    More parsing

--- src/tokens.zig
+++ src/tokens.zig
@@ -16,17 +16,47 @@ pub const Token = union(enum) {
     pub fn parseAlloc(alloc: Allocator, raw: []const u8) !ArrayList(Token) {
         var tokens = ArrayList(Token).init(alloc);
 
-        var parts = std.mem.split(u8, raw, " ");
+        var sections = std.mem.split(u8, raw, "\"");
+        var i: u64 = 0;
+        while (sections.next()) |contents| {
+            if (i % 2 == 1) {
+                try tokens.append(.{ .string = contents });
+            } else {
+                var parts = std.mem.split(u8, raw, " ");
 
-        while (parts.next()) |part| {
-            const token: Token = if (std.mem.eql(u8, part, "["))
-                .left_bracket
-            else if (std.mem.eql(u8, part, "]"))
-                .right_bracket
-            else if (std.mem.eql(u8, part, "true")) .{ .bool = true } else if (std.mem.eql(u8, part, "false")) .{ .bool = false } else .{ .term = part };
-            try tokens.append(token);
+                while (parts.next()) |part| {
+                    const token: Token = parse(part);
+                    try tokens.append(token);
+                }
+            }
         }
 
         return tokens;
+    }
+
+    fn parse(part: []const u8) Token {
+        if (std.mem.eql(u8, part, "[")) {
+            return .left_bracket;
+        }
+        if (std.mem.eql(u8, part, "]")) {
+            return .right_bracket;
+        }
+        if (std.mem.eql(u8, part, "true")) {
+            return .{ .bool = true };
+        }
+        if (std.mem.eql(u8, part, "false")) {
+            return .{ .bool = false };
+        }
+        if (std.mem.startsWith(u8, part, "\\")) {
+            return .{ .deferred_term = part };
+        }
+        if (std.fmt.parseInt(i64, part, 10)) |i| {
+            return .{ .i64 = i };
+        } else |_| {}
+        if (std.fmt.parseFloat(f64, part)) |f| {
+            return .{ .f64 = f };
+        } else |_| {}
+
+        return .{ .term = part };
     }
 };
