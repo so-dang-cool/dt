e9d2b362dc9d92002b7be85829d2fa8d802f2251
ｈｉｌｊｕｓｔｉ <hiljusti@so.dang.cool>
2023-07-04 10:26:52 -0700

    Refactor build script

--- build.sh
+++ build.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
-set -euxo pipefail
+set -euo pipefail
 
 cd "$(dirname "$0")" || exit 1
 project_root="$(pwd)"
 
-compile_zig() {
-    cd "$project_root" || exit 1
+help() {
+    >&2 echo "USAGE: $(basename "$0") [build, test, ...]"
+}
+
+build() {
     zig build
 }
 
-cargo_test() {
+test() {
     cd "$project_root"/old-rust-tests || exit 1
     cargo test
 }
 
-compile_zig && cargo_test
+for cmd in "$@"; do
+    cd "$project_root" || exit 1
+    eval "$cmd"
+done
