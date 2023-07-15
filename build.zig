0217036974560e6c0f2af0f2ae4c6d442ed5498f
Jora Troosh <tensorush@gmail.com>
2023-07-15 18:23:38 +0300

    Remove shell scripts as well

--- build.zig
+++ build.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 
-pub fn build(b: *std.Build) void {
+pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
     const root_source_file = std.Build.FileSource.relative("src/main.zig");
@@ -19,6 +19,31 @@ pub fn build(b: *std.Build) void {
     dt_step.dependOn(&dt_install.step);
     b.default_step.dependOn(dt_step);
 
+    // Dt cross-compiled executables
+    const cross_step = b.step("cross", "Install cross-compiled executables");
+
+    inline for (TRIPLES) |TRIPLE| {
+        const cross = b.addExecutable(.{
+            .name = TRIPLE,
+            .root_source_file = root_source_file,
+            .optimize = optimize,
+            .target = try std.zig.CrossTarget.parse(.{ .arch_os_abi = TRIPLE }),
+        });
+
+        const cross_install = b.addInstallArtifact(cross);
+
+        const cross_tar = b.addSystemCommand(&.{ "tar", "-czvf", TRIPLE ++ ".tgz", switch (cross.target.cpu_arch.?) {
+            .wasm32 => TRIPLE ++ ".wasm",
+            else => TRIPLE,
+        } });
+        cross_tar.cwd = "./zig-out/bin/";
+
+        cross_tar.step.dependOn(&cross_install.step);
+        cross_step.dependOn(&cross_tar.step);
+    }
+
+    b.default_step.dependOn(cross_step);
+
     // Tests
     const test_step = b.step("test", "Run tests");
 
@@ -29,6 +54,35 @@ pub fn build(b: *std.Build) void {
     });
 
     const test_run = b.addRunArtifact(test_exe);
+    test_run.step.dependOn(dt_step);
     test_step.dependOn(&test_run.step);
     b.default_step.dependOn(test_step);
 }
+
+const TRIPLES = .{
+    "aarch64-linux-gnu",
+    "aarch64-linux-musleabi",
+    "aarch64-macos-none",
+    "arm-linux-musleabi",
+    "arm-linux-musleabihf",
+    "mips-linux-gnu",
+    "mips-linux-musl",
+    "mips64-linux-gnuabi64",
+    "mips64-linux-musl",
+    "mips64el-linux-gnuabi64",
+    "mips64el-linux-musl",
+    "mipsel-linux-gnu",
+    "mipsel-linux-musl",
+    "powerpc-linux-gnu",
+    "powerpc-linux-musl",
+    "powerpc64le-linux-gnu",
+    "powerpc64le-linux-musl",
+    "riscv64-linux-gnu",
+    "riscv64-linux-musl",
+    "wasm32-wasi-musl",
+    "x86_64-linux-gnu",
+    "x86_64-linux-musl",
+    "x86_64-macos-none",
+    "x86-linux-gnu",
+    "x86-linux-musl",
+};
