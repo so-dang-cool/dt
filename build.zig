882b1ddc3a9d1e8d1810a686d29fd1556a42d98f
Jora Troosh <tensorush@gmail.com>
2023-07-15 17:20:15 +0300

    Remove more redundancies and prettify

--- build.zig
+++ build.zig
@@ -1,26 +1,34 @@
 const std = @import("std");
 
-pub fn build(b: *std.build.Builder) void {
+pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
     const root_source_file = std.Build.FileSource.relative("src/main.zig");
 
-    const exe = b.addExecutable(.{
+    // Dt executable
+    const dt_step = b.step("dt", "Install dt executable");
+
+    const dt = b.addExecutable(.{
         .name = "dt",
         .root_source_file = root_source_file,
         .optimize = optimize,
         .target = target,
     });
 
-    b.installArtifact(exe);
+    const dt_install = b.addInstallArtifact(dt);
+    dt_step.dependOn(&dt_install.step);
+    b.default_step.dependOn(dt_step);
 
-    const test_step = b.step("test", "Run all tests");
+    // Tests
+    const test_step = b.step("test", "Run tests");
+
     const test_exe = b.addTest(.{
         .root_source_file = root_source_file,
         .optimize = optimize,
         .target = target,
     });
-    const run_test = b.addRunArtifact(test_exe);
 
-    test_step.dependOn(&run_test.step);
+    const test_run = b.addRunArtifact(test_exe);
+    test_step.dependOn(&test_run.step);
+    b.default_step.dependOn(test_step);
 }
